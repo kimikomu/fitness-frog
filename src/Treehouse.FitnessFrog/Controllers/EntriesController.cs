352871021911e16668cddedfbdaa7da8936c3dd5
kimikomu <kimikomu@gmail.com>
2017-06-14 12:40:34 -0700

    Updated "Activity" field to use a drop down list

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -44,8 +44,10 @@ namespace Treehouse.FitnessFrog.Controllers
 		{
 			var entry = new Entry()
 			{
-				Date = DateTime.Today
+				Date = DateTime.Today,
 			};
+
+			ViewBag.ActivitiesSelectListItems = new SelectList(Data.Data.Activities, "Id", "Name");
 	        
 			return View(entry);
         }
@@ -59,6 +61,8 @@ namespace Treehouse.FitnessFrog.Controllers
 
 				return RedirectToAction("Index");
 			}
+
+			ViewBag.ActivitiesSelectListItems = new SelectList(Data.Data.Activities, "Id", "Name");
 
 			return View(entry);
 	    }
